d4ed8af304db7d5fe5fda4ca9f12badf0607a337
Petani Kode <contoh@petanikode.com>
2020-05-04 22:53:44 +0700

    comit

---
+++ Pendaftaran_ainun/app/src/main/java/com/example/pendaftaran_ainun/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.pendaftaran_ainun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button submit;
+    EditText Nama,Email,Telp;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submit = findViewById(R.id.btnok);
+        Nama = findViewById(R.id.editUN);
+        Email = findViewById(R.id.editEmail);
+        Telp = findViewById(R.id.editel);
+
+
+        @@Override
+                Protected void onCreate(Bundle savedInstanceState){
+            ;
+        }
+
+
+    }
+}
